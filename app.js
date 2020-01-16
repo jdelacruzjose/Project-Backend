3a072921af8f2d9d8d98071a1412cbd79f17935d
jdelacruzjose <jdelacruzjose@gmail.com>
2020-01-15 21:41:59 -0500

    latest backend routes

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ const app = express();
 
 app.use('/api/places', placeRoutes);
 
+app.use((error, req, res, next) => {
+  if(res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown message occur'});
+});
+
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -5,13 +5,47 @@ const Fake_Places = [
   {
     id:'p1',
     title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
 
   }
-]
+];
 
-router.get('/', (req, res, next) =>{
-  console.log('Get Req Places')
-  res.json({message: 'Its Working'});
+router.get('/:pid', (req, res, next) =>{
+  const placeId = req.params.pid;
+  
+  const place   = Fake_Places.find(p => {
+    return p.id === placeId
+  });
+
+  if (!place) {
+    const error = new Error('Could not find place for this id.');
+    error.code  = 404;
+    throw error;
+  }
+  
+  res.json({place});
+});
+
+router.get('/user/:uid', (req, res, next) =>{
+  const userID = req.params.uid;
+
+  const place = Fake_Places.find(p => {
+    return p.creator === userID;
+  });
+
+  if (!place) {
+    const error = new Error('Could not find place for this user id.');
+    error.code  = 404;
+    return next(error);
+  }
+  
+  res.json({place});
 });
 
 module.exports = router;
